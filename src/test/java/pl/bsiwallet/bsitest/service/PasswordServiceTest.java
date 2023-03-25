55f10ea6d260875e28a0e80122eed54489fea31d
Daniel Zawadka <zawadkad89@wp.pl>
2023-03-25 08:35:38 +0100

    initial commit - after 1 labs

---
+++ src/test/java/pl/bsiwallet/bsitest/service/PasswordServiceTest.java
@@ -0,0 +1,174 @@
+package pl.bsiwallet.bsitest.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import pl.bsiwallet.bsitest.entities.Password;
+import pl.bsiwallet.bsitest.entities.User;
+import pl.bsiwallet.bsitest.interfaces.PasswordRepository;
+import pl.bsiwallet.bsitest.utils.SecurityUtils;
+import pl.bsiwallet.bsitest.utils.UserSession;
+import pl.bsiwallet.bsitest.wrappers.PasswordRequestWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+class PasswordServiceTest {
+
+    @Autowired
+    private PasswordService passwordService;
+
+    @MockBean
+    private PasswordRepository passwordRepository;
+
+    private User user;
+    private PasswordRequestWrapper passwordWrapper;
+    private Password password;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        user = new User();
+        user.setId(1);
+        user.setPasswordHash("passwordhash");
+        user.setLogin("testuser");
+
+        UserSession userSession = UserSession.getSession();
+        userSession.setUser(user);
+        userSession.setIsValid(true);
+
+        passwordWrapper = new PasswordRequestWrapper();
+        passwordWrapper.setDescription("testdescription");
+        passwordWrapper.setLogin("testlogin");
+        passwordWrapper.setWebAddress("testwebaddress");
+        passwordWrapper.setPassword("testpassword");
+
+        password = new Password();
+        password.setId(1);
+        password.setDescription("testdescription");
+        password.setLogin("testlogin");
+        password.setWebAddress("testwebaddress");
+        password.setPassword(SecurityUtils.encrypt(
+                "testpassword",
+                SecurityUtils.generateKey(SecurityUtils.calculateMD5(user.getPasswordHash()))
+        ));
+        password.setUser(user);
+    }
+
+    @Test
+    void shouldCreateValidPassword() {
+        Mockito.when(passwordRepository.save(Mockito.any(Password.class))).thenReturn(password);
+        ResponseEntity<?> response = passwordService.createPassword(passwordWrapper);
+
+        Password responsePassword = (Password)response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(responsePassword);
+        assertEquals(passwordWrapper.getLogin(), responsePassword.getLogin());
+        assertNotNull(passwordWrapper.getDescription(), responsePassword.getDescription());
+        assertNotNull(passwordWrapper.getWebAddress(), responsePassword.getWebAddress());
+    }
+
+    @Test
+    void shouldThrowErrorInsteadOfCreatingPassword() {
+        String errorMessage = "errorMessage";
+
+        Mockito.when(passwordRepository.save(Mockito.any(Password.class))).thenThrow(new IllegalArgumentException(errorMessage));
+        ResponseEntity<?> response = passwordService.createPassword(passwordWrapper);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals(errorMessage, response.getBody());
+    }
+
+    @Test
+    void shouldReturnSessionUserPasswordList() {
+        List<Password> passwordList = new ArrayList<>();
+        passwordList.add(password);
+
+        Mockito.when(passwordRepository.findByUserId(Mockito.any(Integer.class))).thenReturn(Optional.of(passwordList));
+        ResponseEntity<?> response = passwordService.getSessionUserPasswords();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(passwordList, response.getBody());
+    }
+
+    @Test
+    void shouldReturnEmptyPasswordList() {
+        Mockito.when(passwordRepository.findByUserId(Mockito.any(Integer.class))).thenReturn(Optional.empty());
+        ResponseEntity<?> response = passwordService.getSessionUserPasswords();
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(String.class, response.getBody().getClass());
+    }
+
+    @Test
+    void shouldReturnDecryptedPassword() {
+        Mockito.when(passwordRepository.findById(UserSession.getSession().getUser().getId())).
+                thenReturn(Optional.of(password));
+        ResponseEntity<?> response = passwordService.getDecryptedPassword(UserSession.getSession().getUser().getId());
+        PasswordRequestWrapper responseWrapper = (PasswordRequestWrapper)response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(passwordWrapper.getPassword(), responseWrapper.getPassword());
+        assertEquals(passwordWrapper.getLogin(), responseWrapper.getLogin());
+        assertEquals(passwordWrapper.getDescription(), responseWrapper.getDescription());
+        assertEquals(passwordWrapper.getWebAddress(), responseWrapper.getWebAddress());
+    }
+
+    @Test
+    void shouldNotFindPasswordForDecryption() {
+        Mockito.when(passwordRepository.findById(UserSession.getSession().getUser().getId())).
+                thenReturn(Optional.empty());
+        ResponseEntity<?> response = passwordService.getDecryptedPassword(UserSession.getSession().getUser().getId());
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(String.class, response.getBody().getClass());
+    }
+
+    @Test
+    void shouldReturnErrorInsteadOfDecryptedPassword() {
+        String errorMessage = "message";
+        Mockito.when(passwordRepository.findById(UserSession.getSession().getUser().getId())).
+                thenThrow(new IllegalArgumentException(errorMessage));
+        ResponseEntity<?> response = passwordService.getDecryptedPassword(UserSession.getSession().getUser().getId());
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals(errorMessage, response.getBody());
+    }
+
+    @Test
+    void shouldReturnEditedPassword() {
+        passwordWrapper.setLogin("logink2");
+        Mockito.when(passwordRepository.findById(passwordWrapper.getId())).thenReturn(Optional.of(password));
+        Mockito.when(passwordRepository.save(Mockito.any(Password.class))).thenReturn(password);
+
+        ResponseEntity<?> response = passwordService.editPassword(passwordWrapper);
+        Password responsePassword = (Password)response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(passwordWrapper.getLogin(), responsePassword.getLogin());
+    }
+
+    @Test
+    void shouldReturnErrorInsteadOfEditedPassword() {
+        String errorMessage = "message";
+        Mockito.when(passwordRepository.findById(passwordWrapper.getId()))
+                .thenThrow(new IllegalArgumentException(errorMessage));
+
+        ResponseEntity<?> response = passwordService.editPassword(passwordWrapper);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals(errorMessage, response.getBody());
+    }
+}
---
+++ src/test/java/pl/bsiwallet/bsitest/service/UserServiceTest.java
@@ -0,0 +1,188 @@
+package pl.bsiwallet.bsitest.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import pl.bsiwallet.bsitest.entities.User;
+import pl.bsiwallet.bsitest.interfaces.UserRepository;
+import pl.bsiwallet.bsitest.utils.RandomStringGenerator;
+import pl.bsiwallet.bsitest.utils.SecurityUtils;
+import pl.bsiwallet.bsitest.utils.UserSession;
+import pl.bsiwallet.bsitest.wrappers.PasswordEncryptionUpdateWrapper;
+import pl.bsiwallet.bsitest.wrappers.UserRequestWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    void shouldCreateUser() {
+        UserRequestWrapper wrapper = new UserRequestWrapper();
+        wrapper.setLogin("login");
+        wrapper.setPassword("password");
+        wrapper.setIsHmac(true);
+
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(new User());
+        ResponseEntity<?> response = userService.createUser(wrapper);
+        verify(userRepository, Mockito.times(1)).save(Mockito.any(User.class));
+
+        User responseUser = (User)response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(wrapper.getLogin(), responseUser.getLogin());
+        assertEquals(wrapper.getIsHmac(), responseUser.getKeptAsHash());
+        assertNotNull(responseUser);
+    }
+
+    @Test
+    void shouldThrowErrorInsteadOfCreatingUser() {
+        User user = new User("login","passwordHash", "salt", true);
+        UserRequestWrapper wrapper = new UserRequestWrapper();
+        wrapper.setLogin("login");
+        wrapper.setPassword("password");
+        wrapper.setIsHmac(true);
+        String errorMessage = "Error occurred";
+
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenThrow(new RuntimeException(errorMessage));
+        ResponseEntity<?> response = userService.createUser(wrapper);
+        verify(userRepository, Mockito.times(1)).save(Mockito.any(User.class));
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals(errorMessage, response.getBody());
+    }
+
+    @Test
+    public void shouldReturnAllUserList() {
+        List<User> userList = new ArrayList<>();
+        User user1 = new User("john", true);
+        User user2 = new User("jane", true);
+        userList.add(user1);
+        userList.add(user2);
+
+        Mockito.when(userRepository.findAll()).thenReturn(userList);
+        ResponseEntity<?> response = userService.getAllUsers();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(userList, response.getBody());
+    }
+
+    @Test
+    public void shouldThrowErrorInsteadReturningAllUsers() {
+        Mockito.when(userRepository.findAll()).thenThrow(new RuntimeException("Error occurred"));
+        ResponseEntity<?> response = userService.getAllUsers();
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals("Error occurred", response.getBody());
+    }
+    @Test
+    void shouldReturnUserWhenFindingByLogin() {
+        String login = "login";
+        User user = new User(login, "password", "salt", false);
+
+        Mockito.when(userRepository.findByLogin(Mockito.any(String.class))).thenReturn(Optional.of(user));
+        Optional<User> response = userService.getUserByLogin(login);
+
+        assertNotNull(response);
+        assertEquals(user, response.get());
+    }
+
+    @Test
+    void shouldSuccessfullyLogin() {
+        String login = "testuser";
+        String password = "password123";
+        User user = new User(login, false);
+        user.setSalt(RandomStringGenerator.generateRandomString());
+        user.setPasswordHash(userService.calculatePasswordHash(password, user.getSalt(), user.getKeptAsHash()));
+
+        Mockito.when(userRepository.findByLogin(login)).thenReturn(Optional.of(user));
+        boolean result = userService.login(login, password);
+
+        assertTrue(result);
+        assertTrue(UserSession.getSession().getIsValid());
+        assertEquals(user, UserSession.getSession().getUser());
+    }
+
+    @Test
+    void shouldNotLogin() {
+        String login = "testuser";
+        String password = "password123";
+
+        boolean result = userService.login(login, password);
+
+        assertFalse(result);
+        assertFalse(UserSession.getSession().getIsValid());
+        assertNull(UserSession.getSession().getUser());
+    }
+
+    @Test
+    void shouldChangePassword() {
+        User user = new User("testuser", false);
+        user.setSalt("salt");
+        user.setPasswordHash(SecurityUtils.calculateSha512("password" + user.getSalt()));
+        String oldPassword = user.getPasswordHash();
+        UserSession.getSession().setUser(user);
+
+        PasswordEncryptionUpdateWrapper wrapper = new PasswordEncryptionUpdateWrapper();
+        wrapper.setOldPassword(user.getPasswordHash());
+        wrapper.setNewPassword(SecurityUtils.calculateSha512("newpassword" + user.getSalt()));
+
+        // Create a mock RestTemplate instance
+        RestTemplate restTemplateMock = mock(RestTemplate.class);
+        ResponseEntity<Void> responseEntityMock = new ResponseEntity<>(HttpStatus.OK);
+
+        Mockito.when(restTemplateMock.exchange(
+                Mockito.eq("http://localhost:8080/password/updateEncoding"),
+                Mockito.eq(HttpMethod.PUT),
+                Mockito.any(HttpEntity.class),
+                Mockito.eq(Void.class)
+        )).thenReturn(responseEntityMock);
+
+        UserService userService = new UserService(userRepository);
+        userService.setRestTemplate(restTemplateMock);
+
+        ResponseEntity<?> response = userService.changePassword("newpassword");
+        System.out.println(response);
+        User responseUser = (User) response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotEquals(oldPassword, responseUser.getPasswordHash());
+    }
+
+    @Test
+    void shouldReturnErrorInsteadOfChangingPassword() {
+        String newPassword = "newpassword123";
+        User user = new User("testuser", false);
+        user.setSalt(RandomStringGenerator.generateRandomString());
+        user.setPasswordHash(userService.calculatePasswordHash("oldpassword123", user.getSalt(), user.getKeptAsHash()));
+        UserSession.getSession().setUser(user);
+        Mockito.doThrow(new RuntimeException("Error")).when(userRepository).save(user);
+
+        ResponseEntity<?> response = userService.changePassword(newPassword);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals(user.getPasswordHash(), UserSession.getSession().getUser().getPasswordHash());
+    }
+}
